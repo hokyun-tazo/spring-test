2e4fbf7d1d7d0e357c1229a1219048c4919169cb
(정호균) <wjdghrbs2468@naver.com>
2022-08-03 11:41:36 +0900

    Component Scan

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,7 +1,10 @@
 package hello.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CoreApplication {
     public  static  void main(String[] args)
     {
