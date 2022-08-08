6c58024cd53e7fa820b07aabcd7e1997459356de
(정호균) <wjdghrbs2468@naver.com>
2022-08-08 12:34:06 +0900

    primary and Qualifier

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,13 +8,21 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor       //final붙은 것을 가지고 생성자를 만들어준다, 생성자가 하나면 autowirde를 생략해도 되기 떄문에 아래와같이 생성자가 없는 코드가 완성이 될수 있다
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+
+    //@Qualifier을 사용하는 방법
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("fixDiscountPolicy")*/DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
