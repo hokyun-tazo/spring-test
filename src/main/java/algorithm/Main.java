c29c022365d7f40cefbc1f84d09d68e1bbcc3356
(정호균) <wjdghrbs2468@naver.com>
2022-08-16 16:14:00 +0900

    algo 6-4 LRU

--- src/main/java/algorithm/Main.java
+++ src/main/java/algorithm/Main.java
@@ -1,39 +1,51 @@
 package algorithm;
 import java.util.*;
+
 class Main {
-    public ArrayList<Integer> solution(int[]a,int n) {
-        ArrayList<Integer>answer = new ArrayList<>();
-        for(int i =0;i<a.length;++i)
-        {
-            int temp = a[i];
-            int index = i;
-            for(int x =i;x<a.length;++x)
+    public int[] solution(int s ,int[]a,int n) {
+       int[]answer = new int[s];
+
+        for (int x : a) {
+            int pos = -1;
+            for(int i =0;i<s;++i)
             {
-                if(temp>a[x])
+                if(answer[i]==x)
                 {
-                    temp = a[x];
-                    index = x;
+                    pos = i;
                 }
             }
-            int k = a[i];
-            a[i] = a[index];
-            a[index] = k;
-            answer.add(a[i]);
+            if(pos==-1)
+            {
+                for(int i=s-1;i>=1;--i)
+                {
+                    answer[i]=answer[i-1];
+                }
+            }
+            else
+            {
+                for(int i = pos;i>=1;--i)
+                {
+                    answer[i]=answer[i-1];
+                }
+            }
+            answer[0]=x;
         }
+
+
         return answer;
     }
     public static void main(String[] args){
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
         int n = sc.nextInt();
         int []a = new int[n];
         for(int i =0;i<n;++i)
         {
             a[i]=sc.nextInt();
         }
-        for (Integer x : main.solution(a,n)) {
-            System.out.printf("%d ",x);
-        }
+        main.solution(s,a,n);
+
     }
 }
 
