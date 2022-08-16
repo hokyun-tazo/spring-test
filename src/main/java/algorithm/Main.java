bc53f68831ea110755c5c3db5b2fce2aecd4b0e3
(정호균) <wjdghrbs2468@naver.com>
2022-08-16 12:33:27 +0900

    algo 5-8

--- src/main/java/algorithm/Main.java
+++ src/main/java/algorithm/Main.java
@@ -1,45 +1,80 @@
 package algorithm;
 import java.util.*;
-class Main {
-    public int solution(String str) {
-        int answer = 0;
-        Stack<Integer>bagu = new Stack<>();
-        for(Character x : str.toCharArray())
-        {
-            int temp =0;
-            if(Character.isDigit(x))
-            {
-                bagu.push(x-48);
-            }
-            else
-            {
-                int rt = bagu.pop();
-                int lt = bagu.pop();
-                switch (x){
-                    case'+':
-                        bagu.push(lt+rt);
-                        break;
-                        case'-':
-                        bagu.push(lt-rt);
-                        break;
-                        case'*':
-                        bagu.push(lt*rt);
-                        break;
-                        case'/':
-                        bagu.push(lt/rt);
-                        break;
+class person
+{
+    private int id;
+    private int priority;
 
-                }
+    public person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
+class Main {
+    public int solution(int[]a,int n, int m) {
+        int answer = 0;
+        Queue<person>queue = new LinkedList<>();
+        person temp = new person(0,0);
+        for(int i=0;i<n;++i)
+        {
+            queue.add(new person(i,a[i]));
+            if(i==m) {
+                temp.setId(i);
+                temp.setPriority(a[i]);
             }
         }
-        answer = bagu.peek();
-        return answer;
+        int count = Integer.MIN_VALUE;
+        while (count!=temp.getId())
+        {
+            int k = queue.peek().getPriority();
+            boolean check  = true;
+           for(person x: queue)
+           {
+               if(k<x.getPriority())
+               {
+                   queue.offer(queue.poll());
+                   check = false;
+                   break;
+               }
+           }
+           if(check)
+           {
+               count = queue.peek().getId();
+               queue.poll();
+               ++answer;
+           }
+        }
+
+
+         return answer;
         }
     public static void main(String[] args){
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(main.solution(str));
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int []a = new int[n];
+        for(int i =0;i<n;++i)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println( main.solution(a,n,m));
     }
 }
 
