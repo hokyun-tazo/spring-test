37167723067a6babee7a8e700dfe7fe15ab9d827
(정호균) <wjdghrbs2468@naver.com>
2022-08-17 15:16:05 +0900

    bean LifeCycle CallBack 1

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,51 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient()
+    {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url)
+    {
+        this.url  = url;
+    }
+
+    //서비스 시작히 호출
+    public void connect()
+    {
+        System.out.println("connect: "+ url);
+    }
+
+    public void call (String massage)
+    {
+        System.out.println("call "+url + "massage = "+ massage);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect()
+    {
+        System.out.println("close "+ url);
+    }
+
+    //빈이 생성될때 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //빈이 종료될때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
