d621cde49b2ce457dea5314ab5ba6d2274f2ba56
(정호균) <wjdghrbs2468@naver.com>
2022-08-17 15:22:22 +0900

    Bean LifeCycle CallBack 2

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -34,18 +31,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close "+ url);
     }
 
-    //빈이 생성될때 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    public void init() {
+        System.out.println("init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    //빈이 종료될때 호출
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+
+    public void close()  {
+        System.out.println("close");
         disconnect();
     }
 }
